263901ff60a0abd056d8bcee2563f50758d75089
EfthymisB <thymios_mpaira@hotmail.com>
2023-10-22 23:52:19 +0100

    finalise look

---
+++ media/posters/metadata.js
@@ -0,0 +1,80 @@
+export const metadata = {
+  '1_luther.jpg': {
+    title: 'Luther: the Fallen Sun',
+    year: '2023',
+    description:
+      'Brilliant but disgraced detective John Luther breaks out of prison to hunt down a sadistic serial killer who is terrorising London.',
+    stars: ['Idris Elba', 'Cynthia Erivo', 'Andy Serkis'],
+    imdb_id: 'tt3155298',
+    role: 'Pipeline TD',
+    company: 'One Of Us',
+    location: 'London',
+    credited: true
+  },
+  '2_johnWick.jpg': {
+    title: 'John Wick: Chapter 4',
+    description:
+      'John Wick uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.',
+    stars: ['Keanu Reeves', 'Laurence Fishburne', 'George Georgiou'],
+    imdb_id: 'tt10366206',
+    role: 'Pipeline TD',
+    company: 'One Of Us',
+    location: 'London',
+    credited: true
+  },
+  '3_theFlash.jpg': {
+    title: 'The Flash',
+    description:
+      'Barry Allen uses his super speed to change the past, but his attempt to save his family creates a world without super heroes, forcing him to race for his life in order to save the future.',
+    stars: ['Ezra Miller', 'Michael Keaton', 'Sasha Calle'],
+    imdb_id: 'tt0439572',
+    role: 'Pipeline TD',
+    company: 'One Of Us',
+    location: 'London',
+    credited: false
+  },
+  '4_missionImpossible7.jpg': {
+    title: 'Mission: Impossible - Dead Reckoning Part One',
+    description:
+      'Ethan Hunt and his IMF team must track down a dangerous weapon before it falls into the wrong hands.',
+    stars: ['Tom Cruise', 'Hayley Atwell', 'Ving Rhames'],
+    imdb_id: 'tt9603212',
+    role: 'Pipeline TD',
+    company: 'One Of Us',
+    location: 'London',
+    credited: false
+  },
+  '5_secretInvasion.jpg': {
+    title: 'Secret Invasion',
+    description:
+      'Fury and Talos try to stop the Skrulls who have infiltrated the highest spheres of the Marvel Universe.',
+    stars: ['Samuel L. Jackson', 'Emilia Clarke', 'Don Cheadle'],
+    imdb_id: 'tt13157618',
+    role: 'Pipeline TD',
+    company: 'One Of Us',
+    location: 'London',
+    credited: true
+  },
+  '6_heartOfStone.jpg': {
+    title: 'Heart of Stone',
+    description:
+      'An intelligence operative for a shadowy global peacekeeping agency races to stop a hacker from stealing its most valuable and dangerous weapon.',
+    stars: ['Gal Gaddot', 'Jamie Dore', 'Alia Bhatt'],
+    imdb_id: 'tt13603966',
+    role: 'Pipeline TD',
+    company: 'One Of Us',
+    location: 'London',
+    credited: true
+  },
+  '7_napoleon.jpg': {
+    title: 'Napoleon',
+    description:
+      "The film takes a personal look at Napoleon Bonaparte's origins, and his swift, ruthless climb to emperor viewed through the prism of his addictive, and often volatile, relationship with his wife and one true love, Josephine.",
+    stars: ['Joaquin Phoenix', 'Vanessa Kirby', 'Ludivine Sagnier'],
+    imdb_id: 'tt13287846',
+    role: 'Pipeline TD',
+    company: 'One Of Us',
+    location: 'London',
+    credited: true
+  }
+}
